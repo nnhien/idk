2c2b976862a02c68da3ba01c4c6d93a044b5c2db
Nicholas Nhien <nicholasnhien@gmail.com>
2016-12-26 17:34:16 -0800

    Prime generator

--- Prime.java
+++ Prime.java
@@ -1,18 +1,21 @@
 public class Prime{
    public static void main(String[] args){ 
-      int i = 0;
-      int[] divisors = {1, 2, 3, 5, 7};
+      long i = 1;
+      float output;
+      int[] divisors = {2, 3, 5, 7};
       while(i >= 0){
          for(int j = 0; j < divisors.length; j++){
-	    float output = (i / divisors[0+j]);
-            if(output == (int)output){
+	    output = (i / divisors[0+j]);
+	    if(output == (int)output){
                i++;
 	    }
 	    else{
-	       System.out.print(output);
+	       System.out.print(i + " ");
+	    }
+	    if(j == divisors.length-1){
+	       j = j - j;
 	    }
 	 }
       }
    }
 }
-
